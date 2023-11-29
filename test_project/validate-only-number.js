9d1f7db1428a91f818285aeffd4667634b7a768f
ALLWEB\sambathth <theara.sambath@allweb.com.kh>
2023-11-29 16:49:17 +0700

    testing with vue component

--- test_project/validate-only-number.js
+++ test_project/validate-only-number.js
@@ -1,246 +1,117 @@
-Vue.component('dbinput', {
+Vue.component('db-input', {
     props: {
-        data:'',
-        isButtonDisabled:{
-            type: Boolean,
-            default: false
-        },
-        countdown: 30,
-        initialTime: {
-            type: Number,
-            default: 3
-        },
     },
     vuetify: new Vuetify(),
     data(){
         return {
-            timeRemaining: this.initialTime,
-            clikeRemaining: 3,
-            countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', `Timor L'Este`, 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
-            errorMessages: '',
-            name: null,
-            address: null,
-            city: null,
-            state: null,
-            zip: null,
-            country: null,
-            formHasErrors: false,
+
         }
     },
     template: `
-    <v-app>
-    <v-app-bar
-    absolute
-    color="white"
-    elevate-on-scroll
-    scroll-target="#scrolling-techniques-7"
-  >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-    <v-toolbar-title>Title</v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>mdi-dots-vertical</v-icon>
-    </v-btn>
-  </v-app-bar>
-    
-        <v-container>
-            <v-row justify="center">
-            <v-col
-            cols="12"
-            sm="10"
-            md="8"
-            lg="6"
-            >
-            <v-card ref="form">
-                <v-card-text>
-                <v-text-field
-                    ref="name"
-                    v-model="name"
-                    :rules="[() => !!name || 'This field is required']"
-                    :error-messages="errorMessages"
-                    label="Full Name"
-                    placeholder="John Doe"
-                    required
-                ></v-text-field>
-                <v-text-field
-                    ref="address"
-                    v-model="address"
-                    :rules="[
-                    () => !!address || 'This field is required',
-                    () => !!address && address.length <= 25 || 'Address must be less than 25 characters',
-                    addressCheck
-                    ]"
-                    label="Address Line"
-                    placeholder="Snowy Rock Pl"
-                    counter="25"
-                    required
-                ></v-text-field>
-                <v-text-field
-                    ref="city"
-                    v-model="city"
-                    :rules="[() => !!city || 'This field is required', addressCheck]"
-                    label="City"
-                    placeholder="El Paso"
-                    required
-                ></v-text-field>
-                <v-text-field
-                    ref="state"
-                    v-model="state"
-                    :rules="[() => !!state || 'This field is required']"
-                    label="State/Province/Region"
-                    required
-                    placeholder="TX"
-                ></v-text-field>
-                <v-text-field
-                    ref="zip"
-                    v-model="zip"
-                    :rules="[() => !!zip || 'This field is required']"
-                    label="ZIP / Postal Code"
-                    required
-                    placeholder="79938"
-                ></v-text-field>
-                <v-autocomplete
-                    ref="country"
-                    v-model="country"
-                    :rules="[() => !!country || 'This field is required']"
-                    :items="countries"
-                    label="Country"
-                    placeholder="Select..."
-                    required
-                ></v-autocomplete>
-                </v-card-text>
-                <v-divider class="mt-12"></v-divider>
-                <v-card-actions>
-                <v-btn text>
-                    Cancel
-                </v-btn>
-                <v-spacer></v-spacer>
-                <v-slide-x-reverse-transition>
-                    <v-tooltip
-                    v-if="formHasErrors"
-                    left
-                    >
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                        icon
-                        class="my-0"
-                        v-bind="attrs"
-                        @click="resetForm"
-                        v-on="on"
-                        >
-                        <v-icon>mdi-refresh</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Refresh form</span>
-                    </v-tooltip>
-                </v-slide-x-reverse-transition>
-                <v-btn
-                    color="primary"
-                    text
-                    @click="submit"
-                >
-                    Submit
-                </v-btn>
-                </v-card-actions>
-            </v-card>
-            </v-col>
-        </v-row>
-        </v-container>
-  </v-app>
+        <v-text-field
+        solo
+        label="Append"
+        append-icon="mdi-account"
+        ></v-text-field>
         
+    `,  
+});
+
+Vue.component('db-main',{
+    vuetify: new Vuetify(),
+    template: `
+    <div class="appbar-container">
+        <v-app-bar
+            style="display: flex; justify-content: space-between;"
+            app
+        >
+            <slot></slot>
+        </v-app-bar>
+    </div>
     `,
-    methods: {
-        validateNumber($event) {
-            const keyPressed = String.fromCharCode($event.charCode);
-            if (!/^[0-9+]$/.test(keyPressed)) {
-                $event.preventDefault();
-            }
-        },
-        startCountdown() {
-            this.timer = setInterval(() => {
-              if (this.timeRemaining > 0) {
-                console.log(this.timeRemaining);
-                this.timeRemaining--;
-              } else {
-                clearInterval(this.timer);
-              }
-            }, 1000);
-        },
-        reCountdown(){
-            this.timeRemaining = 3;
-            this.startCountdown();
-            this.clikeRemaining--;
-            if(this.clikeRemaining<=0){
-                this.isButtonDisabled = true;
-            }
-        },
-        validate () {
-            this.$refs.form.validate()
-          },
-          reset () {
-            this.$refs.form.reset()
-          },
-          resetValidation () {
-            this.$refs.form.resetValidation()
-        },
-        addressCheck () {
-            this.errorMessages = this.address && !this.name
-              ? `Hey! I'm required`
-              : ''
-    
-            return true
-          },
-          resetForm () {
-            this.errorMessages = []
-            this.formHasErrors = false
-    
-            Object.keys(this.form).forEach(f => {
-              this.$refs[f].reset()
-            })
-        },
-        submit () {
-            this.formHasErrors = false
-    
-            Object.keys(this.form).forEach(f => {
-              if (!this.form[f]) this.formHasErrors = true
-    
-              this.$refs[f].validate(true)
-            })
-        },  
-    },
-    computed:{
-        form () {
-            return {
-              name: this.name,
-              address: this.address,
-              city: this.city,
-              state: this.state,
-              zip: this.zip,
-              country: this.country,
-            }
-        },
-    },
-    mounted(){
-        this.startCountdown();
-    },
-    watch: {
-        name () {
-            this.errorMessages = ''
-        },
-    }
-        
 });
-new Vue({
-    el: '#root',
+
+Vue.component('db-wireframes',{
+    vuetify: new Vuetify(),
+    data(){
+        return{
+            items: [
+            { header: 'Today' },
+            {
+            avatar: 'https://images.carandbike.com/cms/BMW_S_1000_RR_Winglet_4_cb2f77e0e7.jpg',
+            title: 'Brunch this weekend?',
+            subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+            },
+            { divider: true, inset: true },
+            {
+            avatar: 'https://images.carandbike.com/cms/BMW_S_1000_RR_Winglet_4_cb2f77e0e7.jpg',
+            title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+            subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+            },
+            { divider: true, inset: true },
+            {
+            avatar: 'https://images.carandbike.com/cms/BMW_S_1000_RR_Winglet_4_cb2f77e0e7.jpg',
+            title: 'Oui oui',
+            subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+            },
+            { divider: true, inset: true },
+            {
+            avatar: 'https://images.carandbike.com/cms/BMW_S_1000_RR_Winglet_4_cb2f77e0e7.jpg',
+            title: 'Birthday gift',
+            subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+            },
+            { divider: true, inset: true },
+            {
+            avatar: 'https://images.carandbike.com/cms/BMW_S_1000_RR_Winglet_4_cb2f77e0e7.jpg',
+            title: 'Recipe to try',
+            subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+            },
+      ],}
+    },
+    template: `
+        <v-app id="inspire">
+        <v-app-bar
+        app
+        shrink-on-scroll
+        >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Application</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+        </v-app-bar>
+
+        <v-main>
+        <v-container>
+            <v-row>
+            <template v-for="(item, index) in items">
+                <v-col
+                    v-for="n in items"
+                    :key="n"
+                    cols="4"
+                >
+                    <v-card>
+                    <v-img
+                    :src=item.avatar
+                    height="125"
+                    contain
+                    class="grey darken-4"
+                    ></v-img>
+                    <v-card-title class="text-h6">
+                    height with contain
+                    </v-card-title>
+                </v-card>
+                </v-col>
+                </template>
+            </v-row>
+        </v-container>
+        </v-main>
+    </v-app>
+    `
 });
+
+
